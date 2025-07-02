7793d7101bf031f8c917b817823531733408e8fc
andree <andree1211val@gmail.com>
2025-07-01 19:30:53 -0500

    backend/models:PacienteModel

--- backend_agenda-clinica/agenda-clinica_API-Rest/src/main/java/com/agenda/clinica/agendaclinica_apirest/models/Paciente.java
+++ backend_agenda-clinica/agenda-clinica_API-Rest/src/main/java/com/agenda/clinica/agendaclinica_apirest/models/Paciente.java
@@ -1,4 +1,38 @@
 package com.agenda.clinica.agendaclinica_apirest.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idPaciente;
+
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Paciente(int idPaciente, String nombre, String telefono, String email) {
+        this.idPaciente = idPaciente;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public Paciente() {}
+
+    public int getIdPaciente() { return idPaciente; }
+    public void setIdPaciente(int idPaciente) { this.idPaciente = idPaciente; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
