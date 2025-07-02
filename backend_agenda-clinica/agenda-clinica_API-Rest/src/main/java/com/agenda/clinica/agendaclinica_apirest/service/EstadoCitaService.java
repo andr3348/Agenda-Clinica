da010f98580e1ba99dfc9caf9047dacc150efcd9
andree <andree1211val@gmail.com>
2025-07-02 12:02:20 -0500

    backend: EstadoCitaService

---
+++ backend_agenda-clinica/agenda-clinica_API-Rest/src/main/java/com/agenda/clinica/agendaclinica_apirest/service/EstadoCitaService.java
@@ -0,0 +1,42 @@
+package com.agenda.clinica.agendaclinica_apirest.service;
+
+import com.agenda.clinica.agendaclinica_apirest.models.EstadoCita;
+import com.agenda.clinica.agendaclinica_apirest.repository.EstadoCitaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EstadoCitaService {
+
+    private final EstadoCitaRepository repository;
+    public EstadoCitaService(EstadoCitaRepository repository) { this.repository = repository; }
+
+    public List<EstadoCita> getAll() { return repository.findAll(); }
+
+    public Optional<EstadoCita> getById(Integer id) { return repository.findById(id); }
+
+    public EstadoCita save(EstadoCita estadoCita) { return repository.save(estadoCita); }
+
+    public EstadoCita update(Integer id, EstadoCita estadoCita) {
+        EstadoCita estadoCitaExistente = repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,"Estado de cita no encontrada con el ID: "+id));
+
+        estadoCitaExistente.setNombreEstado(estadoCita.getNombreEstado());
+        estadoCitaExistente.setColorHex(estadoCita.getColorHex());
+
+        return repository.save(estadoCitaExistente);
+    }
+
+    public void delete(Integer id) {
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,"Estado de cita no encontrada con el ID: "+id);
+        }
+        repository.deleteById(id);
+    }
+}
