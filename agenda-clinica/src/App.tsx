51b6400b60150f9e9e9abf9d47f16330a2fa37bb
andree <andree1211val@gmail.com>
2025-07-03 22:41:51 -0500

    fetch y consumo de api

--- agenda-clinica/src/App.tsx
+++ agenda-clinica/src/App.tsx
@@ -1,41 +1,71 @@
 import './App.css'
 import { ScheduleXCalendar, useCalendarApp } from '@schedule-x/react'
-import { createViewWeek, createViewMonthGrid } from '@schedule-x/calendar';
+import { createViewWeek, createViewMonthGrid, type CalendarEventExternal } from '@schedule-x/calendar';
 import '@schedule-x/theme-default/dist/calendar.css';
 import { createEventModalPlugin } from '@schedule-x/event-modal';
 import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop';
 import { createResizePlugin } from '@schedule-x/resize';
+import { useEffect, useState } from 'react';
+import { getCitas, type Cita } from './api/citaApi';
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
   const calendar = useCalendarApp({
     views: [
       createViewWeek(),
       createViewMonthGrid()
     ],
-    events: [
-      {
-        id: 1,
-        title: "New event",
-        start: '2025-01-01 00:00',
-        end: '2025-01-01 02:00',
-        description: 'Cool description'
-      }
-    ],
-    selectedDate: '2025-01-15',
+    selectedDate: new Date().toISOString().split('T')[0],
     plugins: [
       createEventModalPlugin(),
       createDragAndDropPlugin(),
       createResizePlugin()
-    ]
-
+    ],
   });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const citas: Cita[] = await getCitas();
+
+        citas.forEach((cita) => {
+          const event: CalendarEventExternal = {
+            id: cita.idCita.toString(),
+            title: `${cita.paciente.nombre} - ${cita.motivo}`,
+            start: cita.fechaInicio,
+            end: cita.fechaFin,
+            description: `
+                Doctor: ${cita.doctor.nombre}\n
+                Estado: ${cita.estado.nombreEstado}\n
+                Encargado: ${cita.encargado ? cita.encargado.nombre : 'No asignado'}`,
+            backgroundColor: 'Green'
+          };
+
+          if (calendar) {
+            calendar?.events.add(event);
+            //calendar.setTheme('dark');
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+  }, [calendar]);
+
+  if (loading) return <p>Cargando agenda...</p>;
 
   return (
     <>
-    <div>
-      <ScheduleXCalendar calendarApp={calendar} />
-    </div>
+      <div>
+        <ScheduleXCalendar calendarApp={calendar} />
+      </div>
+
     </>
   )
 }
---
+++ agenda-clinica/src/api/citaApi.ts
@@ -0,0 +1,40 @@
+const API_URL: string = import.meta.env.VITE_API_URL;
+
+export interface Cita {
+    idCita: number;
+    fechaInicio: string;
+    fechaFin: string;
+    motivo: string;
+    paciente: {
+        nombre: string;
+        telefono: string;
+        email: string;
+    };
+    doctor: {
+        nombre: string;
+    };
+    estado: {
+        nombreEstado: string;
+        colorHex: string;
+    };
+    encargado?: {
+        nombre: string;
+    };
+    creadoEn: string;
+}
+
+export const getCitas = async (): Promise<Cita[]> => {
+    const response = await fetch(`${API_URL}/citas`);
+    if (!response.ok) throw new Error("Error al obtener las citas");
+    return await response.json();
+};
+
+export const saveCita = async (cita: Partial<Cita>): Promise<Cita> => {
+    const response = await fetch(`${API_URL}/citas`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(cita),
+    });
+    if (!response.ok) throw new Error("Error al crear cita");
+    return await response.json();
+};
